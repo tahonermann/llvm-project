764fac76936b02fb07b6a4382adc5c049559fc7a
Tom Honermann <tom.honermann@intel.com>
2023-10-20 08:43:38 -0700

    Squash: Reworded a comment to address review feedback courtesy of Zahira.

--- clang/include/clang/Basic/TargetInfo.h
+++ clang/include/clang/Basic/TargetInfo.h
@@ -221,9 +221,9 @@ enum OpenCLTypeKind : uint8_t {
 };
 
 // ISO/IEC TS 18661-2, ISO/IEC TR 24733, and C23 decimal floating-point.
-// Multiple encodings for how decimal floating-point values are stored are
-// used in practice. This enumeration provides an enumerator for each known
-// encoding.
+// Multiple encoding forms for the storage of decimal floating-point values
+// have been defined for use on various platforms. This enumeration provides
+// an enumerator for each known encoding.
 enum class DecimalFloatMode : uint8_t {
   BID,  // Binary Integer Decimal.
   DPD,  // Densely Packed Decimal.
