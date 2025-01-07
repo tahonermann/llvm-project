18bf99baeb1889b9dbe58529382ae82efeb296b6
Elizabeth Andrews <elizabeth.andrews@intel.com>
2025-01-07 10:01:00 -0800

    Remove kernel calls for __builtin_sycl_unique_stable_name

--- clang/test/CodeGenSYCL/unique_stable_name_windows_diff.cpp
+++ clang/test/CodeGenSYCL/unique_stable_name_windows_diff.cpp
@@ -2,26 +2,6 @@
 // RUN: %clang_cc1 -triple x86_64-pc-windows-msvc -fsycl-is-device -disable-llvm-passes -fsycl-is-device -emit-llvm %s -o - | FileCheck %s '-D$ADDRSPACE='
 
 
-template<typename KN, typename Func>
-__attribute__((sycl_kernel_entry_point(KN))) void kernel(Func F){
-  F();
-}
-
-template<typename KN, typename Func>
-__attribute__((sycl_kernel_entry_point(KN))) void kernel2(Func F){
-  F(1);
-}
-
-template<typename KN, typename Func>
-__attribute__((sycl_kernel_entry_point(KN))) void kernel3(Func F){
-  F(1.1);
-}
-
-template<typename Func>
-void kernel_wrapper(Func F) {
-  kernel<Func>(F);
-}
-
 int main() {
   auto lambda1 = [](){};
   auto lambda2 = [](int){};
@@ -29,11 +9,9 @@ int main() {
 
   // Ensure the kernels are named the same between the device and host
   // invocations.
-  kernel_wrapper([](){
   (void)__builtin_sycl_unique_stable_name(decltype(lambda1));
   (void)__builtin_sycl_unique_stable_name(decltype(lambda2));
   (void)__builtin_sycl_unique_stable_name(decltype(lambda3));
-  });
 
   // Make sure the following 3 are the same between the host and device compile.
   // Note that these are NOT the same value as eachother, they differ by the
