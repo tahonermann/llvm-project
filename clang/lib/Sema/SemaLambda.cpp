6056e2b36cf219258fe6b67703bf4983fbd15cdd
Tom Honermann <tom.honermann@intel.com>
2025-05-09 10:01:25 -0700

    [SYCL] Remove ItaniumSYCLNumberingContext.
    FIXME: Removing this does destabilize use of lambda closure type names
    as SYCL kernel names. Something like this does seem to be needed.
    
    FIXME: Failed Tests (1):
      Clang :: CodeGenSYCL/kernel-caller-entry-point.cpp

--- clang/lib/Sema/SemaLambda.cpp
+++ clang/lib/Sema/SemaLambda.cpp
@@ -506,13 +506,10 @@ void Sema::handleLambdaNumbering(
       getCurrentMangleNumberContext(Class->getDeclContext());
   if (!MCtx && (getLangOpts().CUDA || getLangOpts().SYCLIsDevice ||
                 getLangOpts().SYCLIsHost)) {
-    // Force lambda numbering in CUDA/HIP as we need to name lambdas following
-    // ODR. Both device- and host-compilation need to have a consistent naming
-    // on kernel functions. As lambdas are potential part of these `__global__`
-    // function names, they needs numbering following ODR.
-    // Also force for SYCL, since we need this for the
-    // __builtin_sycl_unique_stable_name implementation, which depends on lambda
-    // mangling.
+    // Force lambda numbering in CUDA/HIP/SYCL as we need to name lambdas
+    // following ODR. Both device- and host-compilation need to have a
+    // consistent naming on kernel functions. As lambdas are potential part of
+    // these `__global__` function names, they needs numbering following ODR.
     MCtx = getMangleNumberingContext(Class, Numbering.ContextDecl);
     assert(MCtx && "Retrieving mangle numbering context failed!");
     Numbering.HasKnownInternalLinkage = true;
